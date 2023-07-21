8afb84249024751955b8fa263f1718d9f45f8d78
jayavardhan200 <jayavardhanchembakuri@gmail.com>
2023-07-21 19:06:02 +0530

    changes

--- lambdaexpression/LambdaExercise.java
+++ lambdaexpression/LambdaExercise.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class LambdaExercise {
 	public static void main(String[] args) {
-		String[] array = {"Java", "C++", "Ruby", "Python"};
+		String[] array = {"Java", "C", "Ruby", "spring"};
 		System.out.println("Unsorted: " + Arrays.toString(array));
 		Arrays.sort(array, (x, y) -> y.compareTo(x));
 		System.out.println("Reverse sorted: " + Arrays.toString(array));
